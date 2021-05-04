fbd3d3ef7cc622e881128fb497ceb391617b13f5
Ross <59896313+rossSec@users.noreply.github.com>
2021-05-04 20:21:17 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter plaintext: ");
+            string get = Console.ReadLine();
+
+            //Enter hashing algorithm here: E.G Hashing.Sha512(get) <-- Plaintext
+            Hashing.Sha512(get);
+
+            Console.Write("Press enter to continue..");
+            Console.ReadLine();
+        }
+    }
+}
