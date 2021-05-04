ccf78e3abae230d46f97d4cd8e155874fd55ea51
Ross <59896313+rossSec@users.noreply.github.com>
2021-05-04 20:28:17 +0100

    Update Hashing.cs

--- Hashing.cs
+++ Hashing.cs
@@ -25,6 +25,20 @@ namespace Algorithms
             Console.WriteLine("Hashed Value: " + get);
         }
         
+        public static void Sha256(string get)
+        {
+            Console.WriteLine("Prehashed Value: " + get);
+
+            using (SHA256 sha256hash = SHA256.Create())
+            {
+                byte[] textBytes = Encoding.UTF8.GetBytes(get);
+                byte[] hashBytes = sha256hash.ComputeHash(textBytes);
+
+                get = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
+            }
+
+            Console.WriteLine("Hashed Value: " + get);
+        }
         
         //More algorithms being added soon, currently studying for an exam.
 
